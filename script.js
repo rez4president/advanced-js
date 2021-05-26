7246c6f83498369e6f165228271f6c3e4063ed84
Raza Ul Kareem <83599698+rez4president@users.noreply.github.com>
2021-05-26 09:34:55 +0530

    Add files via upload
    Learn Scope and how it functions

---
+++ script.js
@@ -0,0 +1,35 @@
+var fun = 5;
+
+function funFunction (){
+ var fun = "HElloooooo";
+console.log(1, fun);
+}
+function funnerFunction () {
+var fun = "Byeeeee";
+console.log(2, fun);
+}
+
+function funnestFunction () {
+fun = "aaaaaahhhhhh";
+console.log(3, fun);
+}
+console.log("window", fun);
+funFunction();
+funnerFunction();
+funnestFunction();
+
+
+// var fun = 5566;
+
+// function funFunc (){
+// 	var fun = 5;
+// 	console.log(fun);
+// }
+// funFunc();
+
+
+var b = "Can I access this?";
+
+function funnest(){
+	b = "Helloooo"
+}
